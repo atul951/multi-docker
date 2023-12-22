515f6f1b3afb50caa4ff2e27e2cfbce208c87a53
Atul <vicky00065.ak@gmail.com>
2023-12-22 14:56:39 +0530

    initial commit

---
+++ worker/index.js
@@ -0,0 +1,21 @@
+const keys = require('./keys');
+const redis = require('redis');
+
+const redisClient = redis.createClient({
+    host: keys.redisHost,
+    port: keys.redisPort,
+    retry_strategy: () => 1000
+});
+
+const sub = redisClient.duplicate();
+
+function fib(index) {
+    if(index < 2) return 1;
+    return fib(n-1) + fib(n-2);
+}
+
+sub.on('messgae', (channel, message) => {
+    redisClient.hset('values', message, fib(parseInt(index)));
+});
+
+sub.subscribe('insert');
